3e93911190559ac3ee48f830e2c73e807be16104
benoit29200 <contact@autretbenoit.fr>
2017-11-06 16:10:48 +0100

    Revert "Merge branch 'master' of https://github.com/Benoit29200/IDL_TPGBSJ"
    This reverts commit 0c286324f8003c87589fcae4f533055cc5201554, reversing
    changes made to a2d2658fdd4e0851d076fbf257de71bcb0fd0904.

--- babaud/src/suivi/SuiviChauffage.java
+++ babaud/src/suivi/SuiviChauffage.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-package suivi;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SuiviChauffage implements Serializable {
-	
-	protected Map<Integer, SuiviAnnuel> lesAnnees = new HashMap<>();
-	
-	public SuiviChauffage() {
-	}
-
-	public double LireTemperature(int annee, int mois, int jour, int heure,int minute) {
-		return lesAnnees.get(annee).LireTemperature(mois,jour,heure,minute);
-	}
-
-	public void AjoutNouvelleMesure(int annee, int mois, int jour, int heure,int minute,double uneTemperature) {
-		if (lesAnnees.get(annee)==null) {
-			SuiviAnnuel unSuiviAnnuel = new SuiviAnnuel();
-			lesAnnees.put(annee, unSuiviAnnuel);
-		}
-		lesAnnees.get(annee).AjoutNouvelleMesure(mois, jour, heure, minute, uneTemperature);
-	}
-}
-=======
 package suivi;
 
 import java.io.IOException;
@@ -81,4 +54,3 @@ public class SuiviChauffage implements Serializable {
 		oos.writeObject(lesAnnees);
 	}
 }
->>>>>>> e390756aa10c2bdc60c3233bd14eec5fd051d82d
